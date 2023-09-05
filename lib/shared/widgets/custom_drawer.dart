eb155dd794fc7921d257ef7162f868af43ec6345
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2023-09-05 17:31:54 -0300

    UserAccountsDrawerHeader

--- lib/shared/widgets/custom_drawer.dart
+++ lib/shared/widgets/custom_drawer.dart
@@ -7,45 +7,83 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            InkWell(
-              child: const Text('Dados de cadastro'),
-              onTap: () => {
-                Navigator.pop(context),
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const DadosCadPage(
-                            texto: "Meus dados",
-                            dados: ["Nome", "Data de Nascimento"])))
-              },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue[300]),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.blue[600],
+                child: Image.network(
+                    "https://static.wikia.nocookie.net/powerpuff/images/d/df/Macaco_louco.png/revision/latest?cb=20120217214954&path-prefix=pt-br"),
+              ),
+              accountName: Text("Macaco Loouco"),
+              accountEmail: Text("email@email.com")),
+          InkWell(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              width: double.infinity,
+              child: Row(
+                children: [
+                  const Icon(Icons.person),
+                  SizedBox(width: 5),
+                  const Text('Dados de cadastro'),
+                ],
+              ),
             ),
-            const Divider(),
-            const SizedBox(
-              height: 10,
+            onTap: () => {
+              Navigator.pop(context),
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const DadosCadPage(
+                          texto: "Meus dados",
+                          dados: ["Nome", "Data de Nascimento"])))
+            },
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Icon(Icons.book),
+                  SizedBox(width: 10),
+                  const Text('Termos de uso e privacidade'),
+                ],
+              ),
             ),
-            InkWell(
-              child: const Text('Termos de uso e privacidade'),
-              onTap: () => {},
+            onTap: () => {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Icon(Icons.directions_car_filled_sharp),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  const Text('Configurações'),
+                ],
+              ),
             ),
-            const Divider(),
-            const SizedBox(
-              height: 10,
-            ),
-            InkWell(
-              child: const Text('Configurações'),
-              onTap: () => {},
-            ),
-            const Divider(),
-            const SizedBox(
-              height: 10,
-            )
-          ],
-        ),
+            onTap: () => {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          )
+        ],
       ),
     );
   }
